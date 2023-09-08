e4e36b1551cd1436d35bcddb7f0f5aeea3cf4517
Ameer_105 <ameerdamilola@gmail.com>
2023-09-08 19:00:31 +0100

    back-to-top icon⬆

--- js/main.js
+++ js/main.js
@@ -41,6 +41,13 @@ window.addEventListener('scroll', scrollHeader)
 
 
 /*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
